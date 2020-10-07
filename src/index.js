e9f2af85f11af5c901062e58a8767b216d06b5ea
touhidulislamkhan <touhidikhan0@gmail.com>
2020-10-07 23:24:51 +0600

    Full Website Without Authentication

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React.Fragment>
     <App />
-  </React.StrictMode>,
+  </React.Fragment>,
   document.getElementById('root')
 );
 
@@ -15,3 +16,4 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
---
+++ src/redux/actionCreators.js
@@ -0,0 +1,51 @@
+import * as actionTypes from './actionTypes';
+import axios from 'axios';
+
+export const addIngredient = igtype => {
+    return {
+        type: actionTypes.ADD_INGREDIENT,
+        payload: igtype,
+    }
+}
+
+export const removeIngredient = igtype => {
+    return {
+        type: actionTypes.REMOVE_INGREDIENT,
+        payload: igtype,
+    }
+}
+
+export const updatePurchasable = () => {
+    return {
+        type: actionTypes.UPDATE_PURCHASABLE,
+    }
+}
+
+export const resetIngredients = () => {
+    return {
+        type: actionTypes.RESET_INGREDIENTS
+    }
+}
+
+export const loadOrders = (orders) => {
+    return {
+        type: actionTypes.LOAD_ORDERS,
+        payload: orders
+    }
+}
+
+export const orderLoadFailed = () => {
+    return {
+        type: actionTypes.ORDER_LOAD_FAILED
+    }
+}
+
+export const fetchOrders = () => dispatch => {
+    axios.get("https://burger-builder-ae3e1.firebaseio.com/orders.json")
+        .then(response => {
+            dispatch(loadOrders(response.data));
+        })
+        .catch(err => {
+            dispatch(orderLoadFailed());
+        })
+}
